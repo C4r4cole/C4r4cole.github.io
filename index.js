87fcc5cf92c09f6b9c51e648a5491c0929696366
Caracole <francoismoulin37@gmail.com>
2022-08-31 13:56:10 +0200

    nouvel event JS sur id artistNames

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // mouse click
 
 const title = document.querySelector("h1");
+const vents = document.querySelector(".vents");
+const artist = document.getElementById("artistNames");
 
 title.addEventListener("click", () => {
   title.classList.toggle("titleHorde");
+});
+
+window.addEventListener("mousemove", (e) => {
+  vents.style.left = e.pageX + "px";
+  // vents.style.top = e.pageY;
+});
+
+artist.addEventListener("click", () => {
+  artist.classList.toggle("artistNames");
+  artist.style.cursor = "pointer";
 });
